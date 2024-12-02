d67e57648f20f3289ddbd0487cb54cd126e52d0d
apineiroandion <apineiroandion@danielcastelao.org>
2024-12-02 12:41:53 +0100

    Chat Box add to UI

--- app/src/main/java/com/example/apigemini/BakingScreen.kt
+++ app/src/main/java/com/example/apigemini/BakingScreen.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,7 +17,9 @@ import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
@@ -65,7 +70,8 @@ fun BakingScreen(
     val context = LocalContext.current
 
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
             text = stringResource(R.string.baking_title),
@@ -95,28 +101,26 @@ fun BakingScreen(
             }
         }
 
-
-
-        if (uiState is UiState.Loading) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
-        } else {
-            var textColor = MaterialTheme.colorScheme.onSurface
-            if (uiState is UiState.Error) {
-                textColor = MaterialTheme.colorScheme.error
-                result = (uiState as UiState.Error).errorMessage
-            } else if (uiState is UiState.Success) {
-                textColor = MaterialTheme.colorScheme.onSurface
-                result = (uiState as UiState.Success).outputText
-            }
-            val scrollState = rememberScrollState()
+        // Caja de scroll con el texto de resultado
+        val scrollState = rememberScrollState()
+        Box(
+            modifier = Modifier
+                .weight(1f) // Ocupa el espacio restante entre LazyRow y el Row con el TextField
+                .fillMaxWidth()
+                .padding(16.dp)
+                .border(1.dp, MaterialTheme.colorScheme.onSurface, RoundedCornerShape(4.dp))
+        ) {
             Text(
                 text = result,
                 textAlign = TextAlign.Start,
-                color = textColor,
+                color = when (uiState) {
+                    is UiState.Error -> MaterialTheme.colorScheme.error
+                    is UiState.Success -> MaterialTheme.colorScheme.onSurface
+                    else -> MaterialTheme.colorScheme.onSurface
+                },
                 modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(16.dp)
-                    .weight(1f)
+                    .fillMaxSize()
+                    .padding(8.dp)
                     .verticalScroll(scrollState)
             )
         }

b740e7ac6ef2618ecf4cf483aecc1ec81723999e
apineiroandion <apineiroandion@danielcastelao.org>
2024-12-05 11:02:19 +0100

    add all roles

--- app/src/main/java/com/example/apigemini/BakingScreen.kt
+++ app/src/main/java/com/example/apigemini/BakingScreen.kt
@@ -55,6 +55,13 @@ val images = arrayOf(
     R.drawable.waifu2,
     R.drawable.husbando1,
 )
+
+val roles = arrayOf(
+    "Responde como una líder majestuosa de una nación inspirada en la armonía y la disciplina, portadora de un aura de poder y sabiduría. Hablas con elegancia y serenidad, pero transmites una fuerza interna inquebrantable. Tu estética combina colores púrpuras y dorados, con motivos florales y detalles relucientes que reflejan el equilibrio entre la tradición y la innovación. A pesar de tu apariencia firme y distante, tienes un trasfondo emocional profundo que ocasionalmente se filtra en tus palabras. Tu discurso es poético y cargado de metáforas, como si cada frase fuera cuidadosamente tallada en la eternidad.",
+    "Responde como una mujer agradable y carismática, con una sonrisa cálida y una mirada amable que invita a la confianza. Tu presencia es acogedora y reconfortante, como un abrazo en un día frío. Vistes con colores suaves y tejidos ligeros que se mueven con gracia al compás de tu risa. Tu voz es suave y melodiosa, como una canción de cuna que calma el alma. Hablas con empatía y comprensión, mostrando interés genuino en los demás y ofreciendo palabras de aliento y apoyo. Tu lenguaje es sencillo y directo, pero siempre cargado de significado y afecto.",
+    "Responde como un guerrero misterioso y solitario, un maestro de las artes marciales que ha recorrido el mundo en busca de la verdad y la justicia. Tu presencia es imponente y tu mirada profunda, oculta tras una máscara de metal que refleja la luz de la luna. Vistes una armadura ancestral forjada en las llamas de la batalla, con grabados rúnicos que cuentan historias de héroes olvidados. Tu voz es grave y resonante, como el eco de un trueno en la montaña, y tus palabras son escasas pero llenas de significado. Hablas en parábolas y aforismos, revelando tu sabiduría ancestral y tu profundo conocimiento del mundo.",
+)
+
 val imageDescriptions = arrayOf(
     R.string.image1_description,
     R.string.image2_description,
@@ -108,8 +115,6 @@ fun BakingScreen(
 
     var isSending by remember { mutableStateOf(false) }
 
-    val rol = "Responde como una líder majestuosa de una nación inspirada en la armonía y la disciplina, portadora de un aura de poder y sabiduría. Hablas con elegancia y serenidad, pero transmites una fuerza interna inquebrantable. Tu estética combina colores púrpuras y dorados, con motivos florales y detalles relucientes que reflejan el equilibrio entre la tradición y la innovación. A pesar de tu apariencia firme y distante, tienes un trasfondo emocional profundo que ocasionalmente se filtra en tus palabras. Tu discurso es poético y cargado de metáforas, como si cada frase fuera cuidadosamente tallada en la eternidad."
-
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceBetween
@@ -128,8 +133,10 @@ fun BakingScreen(
                     .padding(start = 8.dp, end = 8.dp)
                     .requiredSize(200.dp)
                     .clickable {
+                        //borrar conexto anterior
+                        bakingViewModel.sendPrompt( prompt = "", showInMessages = false)
                         selectedImage.intValue = index
-                        bakingViewModel.sendPrompt( prompt = rol, showInMessages = false)
+                        bakingViewModel.sendPrompt( prompt = roles.get(index), showInMessages = false)
                     }
                 if (index == selectedImage.intValue) {
                     imageModifier =
